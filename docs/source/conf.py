fec3188792079314ece4eb60655cf43dd2747fbd
ellethee <luca800@gmail.com>
2017-06-07 15:41:20 +0200

    doc config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
     'sphinx.ext.coverage',
-    'sphinxcontrib.spelling',
+    # 'sphinxcontrib.spelling',
     'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
